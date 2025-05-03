6c27fd6ed72a1edb4459ce203e306def97af2e67
Furkan Özen <furkanozen@gmail.com>
2025-05-03 12:43:34 +0000

    THe sysyem works for uint64_t and char

--- test/test.cpp
+++ test/test.cpp
@@ -4,16 +4,22 @@
 
 
 int main() {
-    Security mysecurity;
+
+    /*
+    Security<uint64_t> mysecurity;
     int x=5;
     uint64_t a[4];
-    mysecurity.MakeHash(x,a);
-    std::cout<<std::hex<<*a<<std::endl;
-    std::cout<<std::hex<<*(a+1)<<std::endl;;
-    std::cout<<std::hex<<*(a+2)<<std::endl;;
-    std::cout<<std::hex<<*(a+3)<<std::endl;;
 
-    uint64_t y=12;
-    mysecurity.MakeEncryption(y);
-    //mysecurity.yap();
+    uint64_t y=12235433;
+
+    uint64_t* EncryptedData = new uint64_t;
+    uint64_t* Realdata = new uint64_t;
+    mysecurity.MakeEncryption(y,&EncryptedData);
+    std::cout<<"EnctypedData:"<<*EncryptedData<<std::endl;
+    mysecurity.MakeDecryption(*EncryptedData,&Realdata);
+    std::cout<<"Realdata:"<<*Realdata<<std::endl;
+
+    */
+
+
 }
